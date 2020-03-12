72a98f70e8db4b5ad7041da8839bca5bf1d26f32
Zak <zakadmaster@gmail.com>
2020-03-12 14:06:27 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors())
+
+const PORT = process.env.PORT || 9000;
+
+app.get("/api", (req, res) => {
+    res.json("api is running")
+})
+
+app.listen(PORT, () => {
+    console.log(`listening on ${PORT}`)
+})
