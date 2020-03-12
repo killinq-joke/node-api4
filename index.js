33b58620374f526b9354c1f8ab9a7d56c9bb2658
Zak <zakadmaster@gmail.com>
2020-03-12 15:07:38 +0100

    Addition of path.join

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 require("dotenv").config();
 
+const path = require("path");
 const express = require("express");
 const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
-app.use(cors())
+app.use(cors());
+app.use(express.static(path.join(__dirname, "client/build")));
 
 const PORT = process.env.PORT || 9000;
 
 app.get("/api", (req, res) => {
-    res.json("api is running")
-})
+  res.json("api is running");
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
 
 app.listen(PORT, () => {
-    console.log(`listening on ${PORT}`)
-})
+  console.log(`listening on ${PORT}`);
+});
